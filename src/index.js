ef82be11ac5f97922025a27d1cc3dcd99cce197f
AH400796 <A.Hokhman@gmail.com>
2022-11-27 20:41:10 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,149 +1,150 @@
-// import './css/styles.css';
-// import debounce from 'lodash.debounce';
-// import Notiflix from 'notiflix';
-// import { fetchCountries } from './fetchCountries';
-
-// const DEBOUNCE_DELAY = 300;
-// let searchingCountry = null;
-// const { inputCountry, countryList, countryInfoCard } = {
-//   inputCountry: document.querySelector('#search-box'),
-//   countryList: document.querySelector('.country-list'),
-//   countryInfoCard: document.querySelector('.country-info'),
-// };
-
-// inputCountry.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
-
-// function onInput(e) {
-//   searchingCountry = e.target.value.trim();
-//   if (searchingCountry.length < 1) {
-//     clearPage();
-//     return;
-//   }
-//   fetchCountries(searchingCountry).then(data => {
-//     if (!data) {
-//       return;
-//     } else if (data.length > 10) {
-//       Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-//       clearPage();
-//     } else if (data.length > 1) {
-//       makeCountriesList();
-//       makeMarkupForCountriesList(data);
-//     } else if (data.length === 1) {
-//       makeCountryCard();
-//       makeMarkupForCountryCard(data);
-//     }
-//   });
-// }
-
-// function makeMarkupForCountriesList(arr) {
-//   const markup = arr
-//     .map(
-//       ({ name, flags }) => `<li class="country">
-//   <img class="country-flag" src="${flags.svg}" alt="${name.official}" width="70"/>
-//   <p class="country-name">${name.official}</p></li>`
-//     )
-//     .join('');
-
-//   countryList.innerHTML = markup;
-// }
-
-// function makeMarkupForCountryCard(arr) {
-//   const markup = arr.map(
-//     ({ name, flags, capital, languages, population }) =>
-//       `<div class="card-title">
-//   <img class="country-flag-card" src="${flags.svg}" alt="${name.official}" width="70" />
-//   <p class="country-name-card">${name.official}</p>
-// </div>
-// <p class="info"><span class="info-title">Capital: </span>${capital}</p>
-// <p class="info"><span class="info-title">Population: </span>${population}</p>
-// <p class="info"><span class="info-title">Languages: </span>${Object.values(languages).join(', ')}</p>`
-//   );
-
-//   countryInfoCard.innerHTML = markup;
-// }
-
-// function makeCountriesList() {
-//   countryInfoCard.innerHTML = '';
-//   countryInfoCard.classList.remove('card-style');
-//   countryList.classList.add('card-style');
-// }
-
-// function makeCountryCard() {
-//   countryList.innerHTML = '';
-//   countryList.classList.remove('card-style');
-//   countryInfoCard.classList.add('card-style');
-// }
-
-// export const clearPage = function () {
-//   countryList.innerHTML = '';
-//   countryInfoCard.innerHTML = '';
-//   countryList.classList.remove('card-style');
-//   countryInfoCard.classList.remove('card-style');
-// };
-
-import debounce from 'lodash.debounce';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
 import './css/styles.css';
-const DEBOUNCE_DELAY = 300;
-const search = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryContainer = document.querySelector('.country-info');
+import { applicateAPI } from './axios';
 
-search.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
-
-const BASE_URL = `https://restcountries.com/v3.1/name/`;
-const fetchCountries = function (name) {
-  return fetch(`${BASE_URL}${name}?fields=name,capital,population,flags,languages`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .catch(onCountryError);
+const { searchForm, imageGallery, guard } = {
+  searchForm: document.querySelector('#search-form'),
+  imageGallery: document.querySelector('.gallery'),
+  guard: document.querySelector('.js-guard'),
 };
 
-function onInput(e) {
-  const nameCountry = e.target.value.trim();
-  if (!nameCountry) {
-    clearMarkup(countryList);
-    clearMarkup(countryContainer);
-    return;
+const simpleligthbox = new SimpleLightbox('.gallery a', { loop: false });
+const perPage = 40;
+let quantity = 0;
+let page = 1;
+let observer = null;
+const options = {
+  root: null,
+  rootMargin: '600px',
+  threshold: 1.0,
+};
+
+searchForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  quantity = 0;
+  imageGallery.innerHTML = '';
+  if (page > 1) {
+    observer.unobserve(guard);
   }
-  fetchCountries(nameCountry).then(data => {
-    renderCountryList(data);
-    renderCountryinfo(data);
+  page = 1;
+
+  const searchQuery = e.target.elements.searchQuery.value.trim();
+  observer = new IntersectionObserver(onLoad, options);
+  observer.observe(guard);
+
+  function onLoad(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        applicateAPI(searchQuery, page, perPage)
+          .then(response => {
+            console.log('start: ', page);
+
+            if (response.data.hits.length < 1) {
+              console.log(response.data.hits);
+              throw new Error();
+            }
+            addMoreImages(response.data.hits);
+            if (page > 1) {
+              smoothImagesScroll();
+            }
+            if (page === 1) {
+              notifySuccess(response);
+            }
+            if (page === Math.ceil(response.data.totalHits / perPage)) {
+              notifyInfo();
+              observer.unobserve(guard);
+            }
+            changeFormOpacity();
+            page += 1;
+          })
+          .catch(error => {
+            notifyFailure();
+            observer.unobserve(guard);
+          })
+          .then(() => simpleligthbox.refresh());
+      }
+    });
+  }
+}
+
+function createMarkup(array) {
+  return array
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card"><div class="thumb"><a class="gallery-item" href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" /></a></div>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+            <span>${likes}</span>
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+            <span>${views}</span>
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+            <span>${comments}</span>
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+            <span>${downloads}</span>
+          </p>
+        </div>
+      </div>`
+    )
+    .join('');
+}
+
+function addMoreImages(array) {
+  imageGallery.insertAdjacentHTML('beforeend', createMarkup(array));
+}
+
+function smoothImagesScroll() {
+  const { height: cardHeight } =
+    imageGallery.firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
   });
 }
 
-function renderCountryList(countries) {
-  const markup = countries
-    .map(
-      country => `<li class="list">
-        <img src = "${country.flags.svg}" alt = "A flag" width="40px">
-        <span>${country.name.official}</span>
-    </li >`
-    )
-    .join('');
-  countryList.innerHTML = markup;
+function changeFormOpacity() {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      searchForm.classList.add('page-is-scrolled');
+    } else {
+      searchForm.classList.remove('page-is-scrolled');
+    }
+  });
 }
 
-function renderCountryinfo(countries) {
-  const markup = countries
-    .map(
-      country => `<img src="${country.flags.svg}" alt="A flag" width="40px"></img><span> ${country.name.official}</span>
-      <p><b>Capital:</b> ${country.capital}</p>
-      <p><b>Popuation:</b> ${country.population}</p>
-      <p><b>Languages:</b> ${Object.values(country.languages).join(', ')}</p>`
-    )
-    .join('');
-  countryContainer.innerHTML = markup;
+function notifySuccess(response) {
+  Notiflix.Notify.success(
+    `Hooray! We found: ${response.data.total} images,
+       available for display: ${response.data.totalHits} images.`
+  );
 }
 
-function onCountryError() {
-  Notiflix.Notify.failure('Oops, there is no country with that name');
+function notifyFailure() {
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
 }
 
-function clearMarkup(section) {
-  section.innerHTML = '';
+function notifyInfo() {
+  Notiflix.Notify.info(
+    "We're sorry, but you've reached the end of search results"
+  );
 }
